9db2e41007dbc57163ad0f07030c004115d799be
Daithí <davecoll3@gmail.com>
2023-04-27 11:48:22 +0000

    feat: updated basket context

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+from django.shortcuts import get_object_or_404
 from django.conf import settings
+from products.models import Product
 
 
 def basket_contents(request):
@@ -6,11 +9,23 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    delivery = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total == 0:
         delivery = 0
     else:
-        delivery = 3.99
+        delivery = Decimal(3.99)
 
     grand_total = delivery + total
 
--- products/models.py
+++ products/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(
-        'Category', null=True, on_delete=models.SET_NULL
+        'Category', on_delete=models.PROTECT
     )
     sku = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)

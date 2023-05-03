1e948f7d09d23c9062912d381f13e3b0de9386e3
Daithí <davecoll3@gmail.com>
2023-05-03 12:01:26 +0000

    feat: added checkout view and template

--- got_no_beans/settings.py
+++ got_no_beans/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'got_no_beans.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

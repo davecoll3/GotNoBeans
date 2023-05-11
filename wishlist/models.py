8be4096aebefc23a68d1d62bb21ec861c8429075
Daithí <davecoll3@gmail.com>
2023-05-11 15:32:06 +0000

    Feat: created wishlist views

--- wishlist/models.py
+++ wishlist/models.py
@@ -7,7 +7,7 @@ class Wishlist(models.Model):
     # Allows users to build a product wishlist
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=False, blank=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False)
-    added_on = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.product.name
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from products.models import Product
+from profiles.models import UserProfile
+from .models import Wishlist
+
+
+@login_required
+def wishlist(request):
+    # A view that renders the wishlist contents page
+
+    return render(request, 'wishlist/wishlist.html')
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    # Add product to the wishlist
+    user = UserProfile.objects.get(user=request.user)
+    product = get_object_or_404(product, pk=product_id)
+    existing_wishlist = wishlist.objects.filter(user=user, product=product).exists()
+
+    if not existing_wishlist:
+        wishlist_item = Wishlist.objects.create(
+            user=user,
+            product=product
+        )
+        messages.success(request, f'You have added {wishlist_item} to your wishlist.')
+        return redirect(reverse('product_detail', args=[product.id]))
+    else:
+        wishlist_item = Wishlist.objects.get(
+            user=user,
+            product=product
+        )
+        wishlist_item.delete()
+        messages.info(request, 'Removed from wishlist')
+        return redirect(reverse('product_detail', args=[product.id]))

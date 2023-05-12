abb4a77008648f5a8099fa20dacb617acc93b7b2
Daithí <davecoll3@gmail.com>
2023-05-12 10:57:40 +0000

    Fix: updated model due to ForeignKey error

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Wishlist
+from .models import Wishlist, WishlistItem
 
 admin.site.register(Wishlist)
+admin.site.register(WishlistItem)
--- wishlist/models.py
+++ wishlist/models.py
@@ -6,15 +6,15 @@ from profiles.models import UserProfile
 
 class Wishlist(models.Model):
     # Link wishlist product to user
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist')
 
     def __str__(self):
-        return self.user.username
+        return f'Wishlist ({self.user})'
 
 
 class WishlistItem(models.Model):
     # Items in wishlist
-    wishlist = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=False, blank=False, related_name='user_wishlist')
+    wishlist = models.OneToOneField(Wishlist, on_delete=models.CASCADE, null=False, blank=False, related_name='my_wishlist')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False, related_name='wishlist_products')
     date = models.DateTimeField(auto_now_add=True)
 

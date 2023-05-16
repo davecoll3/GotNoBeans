b82dc8f1095ca8dbcc5d1e0dad67cc5b1fec430b
Daithí <davecoll3@gmail.com>
2023-05-16 13:14:27 +0000

    Feat: added recipes views and templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+
+def all_recipes(request):
+    """ A view to return all brewing recipies """
+
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes': recipes,
+    }
+
+    return render(request, 'recipes/recipes.html', context)

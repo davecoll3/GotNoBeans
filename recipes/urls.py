6ddfa0b5e54e1d5e47979a15e905df01285cb4ef
Daithí <davecoll3@gmail.com>
2023-05-18 12:53:08 +0000

    Feat: added bootstrap accordion to profile template

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_recipes, name='recipes'),
-    path('<int:recipe_id>', views.recipe_detail, name='recipe_detail'),
+    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('add/', views.add_recipe, name='add_recipe'),
     path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
     path('delete/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),

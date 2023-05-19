d19ad1450df9bf239bc1b06d95f84a78c9fb7fda
Daithí <davecoll3@gmail.com>
2023-05-19 20:52:08 +0000

    Style: updated styling of search input and button on base template

--- got_no_beans/settings.py
+++ got_no_beans/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['got-no-beans.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('HEROKU_HOST'), os.environ.get('DOMAIN_HOST')]
 
 
 # Application definition
